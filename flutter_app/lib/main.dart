43f24d42b30c33599f1b011bc84bccb8f4225867
lukewilliamson98 <47017444+lukewilliamson98@users.noreply.github.com>
2019-10-16 21:39:53 -0500

    changes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Conversational Advisor',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,9 +19,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Atos Learning and Development Advisor'),
     );
   }
 }
